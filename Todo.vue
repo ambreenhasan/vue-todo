9ae4336a68efd44d52aefa032aeffe681dfe252b
Ambreen Hasan <ambreenhasan@users.noreply.github.com>
2017-05-08 15:32:43 -0700

    Create Todo.vue

---
+++ Todo.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="todo-item">
+    <ol>
+      <li v-for='option in options'>
+        {{ option }}
+      </li>
+    </ol>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        opened: false // data needs to return something, so we are returning a thing.
+      }
+    },
+    props: {
+      value: {
+        required: true
+      },
+      options: {
+        type: Array,
+        required: true
+      },
+    }
+  }
+</script
